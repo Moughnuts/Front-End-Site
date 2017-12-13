7c2abab7cf8e81b2bd2dfa13eca6265a4d684b2c
Moughnuts <muazmib@gmail.com>
2017-12-13 07:24:23 +0000

    made changes to the html, CSS and JS, mobile shoudl work better.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
+document.getElementById('open').addEventListener('click', openNav);
+
 function openNav() {
     document.getElementById("mobile").style.width = "100%";
 }
+
+document.getElementById('close').addEventListener('click', closeNav);
 
 function closeNav() {
     document.getElementById("mobile").style.width = "0%";
